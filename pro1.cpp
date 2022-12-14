322701153634195319e8777eef1b74031ef56fb0
Muhammad Zaid <sheikhzaid768@gmail.com>
2022-12-14 10:51:47 +0500

    Shapes Drawn in Cmd using C++

---
+++ pro1.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+
+main()
+{
+  cout<<"My name is MOHAMMAD ZAID"<<endl;
+  cout<<"This is our first programming lab" <<endl;
+  cout<<"Our class is at N12"<<endl;
+}
